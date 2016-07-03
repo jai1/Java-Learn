4dcd8c323543be305e4d927a6b731773e28cbb53
jai1 <jai1@yahoo-inc.com>
2016-07-03 03:07:52 -0700

    Re organized

--- src/Solutions/02_Lambdas/FunctionalInterface_solution/src/com/example/javase8/interfaces/InterfaceWithArgs.java
+++
@@ -1,6 +0,0 @@
-package com.example.javase8.interfaces;
-
-@FunctionalInterface
-public interface InterfaceWithArgs {
-	public void calculate(int value1, int value2);
-}
--- src/Solutions/02_Lambdas/FunctionalInterface_solution/src/com/example/javase8/interfaces/SimpleInterface.java
+++
@@ -1,6 +0,0 @@
-package com.example.javase8.interfaces;
-
-@FunctionalInterface
-public interface SimpleInterface {
-	public void doSomething();
-}
